4de56b08f83efb02560579b8a72297c5d3b363d3
Prince Narteh <prinarteh@gmail.com>
2022-10-13 11:28:53 +0000

    change from getStaticProps to getServerSideProps

--- pages/posts/[postId].jsx
+++ pages/posts/[postId].jsx
@@ -1,18 +1,7 @@
 import React from "react";
 import { getPosts } from "../../utils/getPosts";
 
-export async function getStaticPaths() {
-  const res = await getPosts();
-
-  const paths = res.map((post) => ({ params: { postId: String(post._id) } }));
-
-  return {
-    paths,
-    fallback: false,
-  };
-}
-
-export async function getStaticProps(context) {
+export async function getServerSideProps(context) {
   const post = await getPosts(context.params.postId);
 
   return {
